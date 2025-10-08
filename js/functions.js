3e1b16cbab459bf03f7356e144b5cfe1fe60bc98
n3dusa <n3dusa00@students.oamk.fi>
2025-10-08 17:00:18 +0300

    Answers added

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ function showAnswers() {
         "Azure Monitoria käytetään sovellusten ja resurssien valvontaan, lokien keräämiseen ja vianmääritykseen.",
         "Azure Key Vault suojaa ja hallinnoi salaisuuksia, kuten salasanoja ja API-avaimia, turvallisesti.",
         "Azure Virtual Machines mahdollistaa virtuaalikoneiden ajamisen pilvessä ilman omaa laitteistoa.",
-        "Azure App Service on hallittu palvelu web-sovellusten julkaisuun ja ajamiseen ilman palvelinten ylläpitoa."
+        "Azure App Service on hallittua palvelua web-sovellusten julkaisuun ja ajamiseen ilman palvelinten ylläpitoa."
     ];
 
     for (let i = 1; i <= answers.length; i++) {
