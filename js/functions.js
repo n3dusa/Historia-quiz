49143dfc89714db05c3fbd670a05ff1bef5f14a8
n3dusa <n3dusa00@students.oamk.fi>
2025-10-08 14:21:47 +0300

    Toinen versio: vastaukset lisätty ja sovellus toimii

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,13 @@
+function showAnswers() {
+    const answers = [
+        "Ensimmäinen Rooman keisari oli Augustus (Octavianus).",
+        "Bastiljin vallankumous tapahtui Ranskassa vuonna 1789.",
+        "\"Ihmeiden matka\" (Travels of Marco Polo) kirjoitti Marco Polo.",
+        "Toinen maailmansota alkoi vuonna 1939.",
+        "Sparta tunnettiin sotilaistaan ja se sijaitsi antiikin Kreikassa."
+    ];
 
+    for (let i = 1; i <= answers.length; i++) {
+        document.getElementById(`answer${i}`).textContent = answers[i-1];
+    }
+}
